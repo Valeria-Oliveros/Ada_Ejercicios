5a72b3e567b109f8de966339c3e8f15467ca1c95
Valeria-Oliveros <corredorv@uninorte.edu.co>
2025-08-27 23:12:55 -0500

    decoración ✨

--- Clase8.js
+++ Clase8.js
@@ -11,7 +11,7 @@ const prompt = require('prompt-sync')();
 
 const num1 = parseFloat(prompt('Ingrese un número cualquiera: '));
 let doble = num1 * 2;
-console.log(`El doble de ${num1} es ${doble}`);
+console.log(`El doble de ${num1} es ${doble} ✨`);
 
 // Ejercicio 2: Verifica si un número es positivo, negativo o cero
 
@@ -24,23 +24,23 @@ if (num2 > 0) {
 } else {
     res1 = 'Cero';
 }   
-console.log(`El número ${num2} es ${res1}`);
+console.log(`El número ${num2} es ${res1} ✨`);
 
 // Ejercicio 3: Suma de dos número ingresados por el usuario
 
 const num3_1 = parseFloat(prompt('Ingrese el primer número: '));
 const num3_2 = parseFloat(prompt('Ingrese el segundo número: '));
 let suma = num3_1 + num3_2;
-console.log(`La suma de (${num3_1} + ${num3_2}) es igual a ${suma}`);
+console.log(`La suma de (${num3_1} + ${num3_2}) es igual a ${suma} ✨`);
 
 // Ejercicio 4: Determina si un número es par o impar
 
 const num4 = parseFloat(prompt('Ingrese un número entero: '));
 let res2 = num4 % 2;
 if (res2 != 0) {
-    console.log(`El número ${num4} es impar`);
+    console.log(`El número ${num4} es impar ✨`);
 } else {
-    console.log(`El número ${num4} es par`);
+    console.log(`El número ${num4} es par ✨`);
 }
 
 // Ejercicio 5: Saludo personalizado
